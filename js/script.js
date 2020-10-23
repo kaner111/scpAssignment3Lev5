744304e8103605f369cf97a172d0b270b629053e
kaner111 <70300039+kaner111@users.noreply.github.com>
2020-10-24 10:46:21 +1300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("div").click(function(){
+      $("#div1").delay(100).fadeIn();
+      $("#div2").delay(350).fadeIn();
+      $("#div3").delay(550).fadeIn();
+    });
+  });
